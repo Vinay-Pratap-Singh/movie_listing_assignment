b7e8ac71cdfefe75f65143ecd5f4b946fb2a04c2
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-08-21 11:03:58 +0530

    setup: redux toolkit setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
+import axiosInstance from "./helper/axiosInstance";
 
 const Home = () => {
+  useEffect(() => {
+    const res = axiosInstance.get("/");
+  }, []);
   return (
     <div>
       {/* adding the header */}
